60c5a8f9defbd8b282c3553bfc85c8482efd3977
Ismatullo Mukhamedzhanov <ismatullo.mukhamedzhanov_2022@UCA.corp>
2021-08-09 16:36:21 +0500

    Second commit. Removed comments in code

--- HomeWork1Solution/Program.cs
+++ HomeWork1Solution/Program.cs
@@ -9,65 +9,65 @@ namespace HomeWork1Solution
         {
             // Question 1 Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то
             // есть квадратный корень из их произведения: √a·b.
-            // Console.WriteLine("Enter first number = ");
-            // double n1 = double.Parse(Console.ReadLine());
-            // Console.WriteLine("Enter second number = ");
-            // double n2 = double.Parse(Console.ReadLine());
-            // double result = Math.Sqrt(n1 * n2);
-            // Console.WriteLine($"Your result is {result}", result);
+            Console.WriteLine("Enter first number = ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number = ");
+            double n2 = double.Parse(Console.ReadLine());
+            double result = Math.Sqrt(n1 * n2);
+            Console.WriteLine($"Your result is {result}", result);
             // Question 2
             // Даны три точки A, B, C на числовой оси. Найти длины отрезков AC и BC и их
             // сумму.
-            // Console.WriteLine("Enter first point = ");
-            // double a = double.Parse(Console.ReadLine());
-            // Console.WriteLine("Enter second point = ");
-            // double b = double.Parse(Console.ReadLine());
-            // Console.WriteLine("Enter third point = ");
-            // double c = double.Parse(Console.ReadLine());
-            // double ac = a - c;
-            // Console.WriteLine($"AC = {ac}" , ac);
-            // double bc = b - c;
-            // Console.WriteLine($"BC = {bc}" , bc);
-            // double sum = Math.Abs(ac) + Math.Abs(bc);
-            // Console.WriteLine($"Sum of line segments is {sum}" , sum); 
+            Console.WriteLine("Enter first point = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second point = ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third point = ");
+            double c = double.Parse(Console.ReadLine());
+            double ac = a - c;
+            Console.WriteLine($"AC = {ac}" , ac);
+            double bc = b - c;
+            Console.WriteLine($"BC = {bc}" , bc);
+            double sum = Math.Abs(ac) + Math.Abs(bc);
+            Console.WriteLine($"Sum of line segments is {sum}" , sum); 
             // Question 3
             // Найти расстояние между двумя точками с заданными координатами (x1,y1) и
             // (x2,y2) на плоскости. Расстояние вычисляется по формуле √((x2 — x1)2 + (y2 —
             // y1)2)
-            // Console.WriteLine("x1 = ");
-            // double x1 = double.Parse(Console.ReadLine());
-            // Console.WriteLine("x2 = ");
-            // double x2 = double.Parse(Console.ReadLine());
-            // Console.WriteLine("y1 = ");
-            // double y1 = double.Parse(Console.ReadLine());
-            // Console.WriteLine("y2 = ");
-            // double y2 = double.Parse(Console.ReadLine());        
-            // double result;
-            // result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2-y1),2));
-            // Console.WriteLine($"Distance is {result}" , result);
+            Console.WriteLine("x1 = ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("x2 = ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("y1 = ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("y2 = ");
+            double y2 = double.Parse(Console.ReadLine());        
+            double result;
+            result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2-y1),2));
+            Console.WriteLine($"Distance is {result}" , result);
             // Question 4 
             // Дано двузначное число. Вывести число, полученное при перестановке цифр
             // исходного числа.
-            // Console.WriteLine("Number = ");
-            // int x = int.Parse(Console.ReadLine());
-            // int x1 = x / 10;
-            // int x2 = x % 10;
-            // int result = x2 * 10 + x1;
-            // Console.WriteLine($"Result is {result}" , result);
+            Console.WriteLine("Number = ");
+            int x = int.Parse(Console.ReadLine());
+            int x1 = x / 10;
+            int x2 = x % 10;
+            int result = x2 * 10 + x1;
+            Console.WriteLine($"Result is {result}" , result);
             // Question 5
             // Дано двузначное число. Вывести число, полученное при перестановке цифр
             // исходного числа.
-            // Console.WriteLine("N = ");
-            // int N = int.Parse(Console.ReadLine());
-            // int result = N / 60;
-            // Console.WriteLine($"Result is {result} minutes" , result);
+            Console.WriteLine("N = ");
+            int N = int.Parse(Console.ReadLine());
+            int result = N / 60;
+            Console.WriteLine($"Result is {result} minutes" , result);
             // Question 6
             // Дни недели пронумерованы следующим образом: 0 — воскресенье, 1 — понедельник, 2 — вторник, …, 6 — суббота. Дано целое число K, лежащее в диапазоне 1–365. 
             // Определить номер дня недели для K-го дня года, если известно, что в этом году 1 января было понедельником.
-            // Console.WriteLine("K = ");
-            // int K = int.Parse(Console.ReadLine());
-            // int result = K % 7;
-            // Console.WriteLine($"Number of the dayweek is {result}" , result);
+            Console.WriteLine("K = ");
+            int K = int.Parse(Console.ReadLine());
+            int result = K % 7;
+            Console.WriteLine($"Number of the dayweek is {result}" , result);
         }
     }
 }
